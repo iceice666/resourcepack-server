759146a3a6098f1c4727f23c291bd12dac93b3ea
iceice666 <iceice666@outlook.com>
2023-10-11 21:16:26 +0800

    Mod info setup

--- src/main/java/net/iceice666/mixin/Mixin.java
+++ src/main/java/net/iceice666/mixin/Mixin.java
@@ -1,13 +1,12 @@
-package com.example.mixin;
+package net.iceice666.mixin;
 
 import net.minecraft.server.MinecraftServer;
-import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
-public class ExampleMixin {
+@org.spongepowered.asm.mixin.Mixin(MinecraftServer.class)
+public class Mixin {
 	@Inject(at = @At("HEAD"), method = "loadWorld")
 	private void init(CallbackInfo info) {
 		// This code is injected into the start of MinecraftServer.loadWorld()V
